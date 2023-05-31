d291a2da98a0bcf2ea292982e55f1134169375c7
platisam <platisam@gmail.com>
2023-05-31 18:24:44 +0200

    added access modifiers: public, private and readonly

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 class Invoice {
-  client: string;
-  details: string;
-  amount: number;
+  readonly client: string;
+  private details: string;
+  public amount: number;
 
   constructor(c: string, d: string, a: number) {
     this.client = c;
@@ -16,6 +16,14 @@ class Invoice {
 
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach((inv) => {
+  console.log(inv.client, inv.amount, inv.format());
+});
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
