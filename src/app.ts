73bb60862108494cd98c60c7a0eb782b0bcb54e3
platisam <platisam@gmail.com>
2023-05-31 22:17:56 +0200

    created doc object based on the Invoice/Payment class

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,29 @@
 import { Invoice } from "./classes/Invoice";
+import { Payment } from "./classes/Payment";
+import { HasFormatter } from "./interfaces/HasFormatter";
 
-const invOne = new Invoice("mario", "work on the mario website", 250);
-const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+//
+// docOne = new Invoice("yoshi", "web work", 250);
+// docTwo = new Payment("mario", "plumbing work", 200);
+//
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+//
+// console.log(docs);
 
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-
-invoices.forEach((inv) => {
-  console.log(inv.client, inv.amount, inv.format());
-});
+// const invOne = new Invoice("mario", "work on the mario website", 250);
+// const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+//
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+//
+// invoices.forEach((inv) => {
+// console.log(inv.client, inv.amount, inv.format());
+// });
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
@@ -21,5 +35,12 @@ const amount = document.querySelector("#amount") as HTMLInputElement;
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
-  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+  let doc: HasFormatter;
+  if (type.value === "invoice") {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+
+  console.log(doc);
 });
